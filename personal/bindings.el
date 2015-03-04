ec0ffec10a1ee96b78e68d8601db7591e2ac0179
Vincent Roy <vincentroy8@gmail.com>
2015-03-03 21:30:21 -0400

    Update rspec mode bindings for my brain.

--- personal/bindings.el
+++ personal/bindings.el
@@ -42,11 +42,11 @@
 ;;     http://barelyenough.org/projects/rspec-mode/
 ;;     https://github.com/pezra/rspec-mode/
 ;;
-(global-set-key (kbd "s-i C-v") 'rspec-verify)
+(global-set-key (kbd "s-i C-f") 'rspec-verify)
 (global-set-key (kbd "s-i C-a") 'rspec-verify-all)
 (global-set-key (kbd "s-i C-t") 'rspec-toggle-spec-and-target)
 (global-set-key (kbd "s-i C-r") 'rspec-rerun)
-(global-set-key (kbd "s-i C-s") 'rspec-verify-single)
+(global-set-key (kbd "s-i s-i") 'rspec-verify-single)
 
 ;; http://stackoverflow.com/a/4513683
 (keyboard-translate ?\C-i ?\s-i)
