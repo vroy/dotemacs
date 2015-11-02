38106c7911890d9450523ed923669229c32cf10d
Vincent Roy <vincent.roy@emc.com>
2015-11-02 09:50:38 -0400

    Update to use brew installed cask.

--- init.el
+++ init.el
@@ -1,4 +1,7 @@
-(require 'cask "~/.cask/cask.el")
+(let ((default-directory "/usr/local/share/emacs/site-lisp/"))
+  (normal-top-level-add-subdirs-to-load-path))
+
+(require 'cask)
 (cask-initialize)
 
 (require 'god-mode)
